bdb00ca1ec8cb0fbb27f04af37977c0fe8f76cb3
Kirunin <kirunin1@yandex.com>
2023-10-30 19:20:44 +0300

    modernization header buttons (adaptiv)

--- js/header.js
+++ js/header.js
@@ -4,8 +4,13 @@ const menu = document.querySelector('#menu');
 menuButton.addEventListener('click', () => {
     if (menu.classList.contains('disp') == true ) { 
         menu.classList.remove('disp');
+        menuButton.style.backgroundColor = 'white';
+        menuButton.style.color = 'black';
+        menuButton.style.borderRadius = '15px';
     } else {
         menu.classList.add ('disp');
+        menuButton.style.backgroundColor = '';
+        menuButton.style.color = '';
     }
 })
 
@@ -15,7 +20,12 @@ const nav = document.querySelector('#nav');
 navButton.addEventListener('click', () => {
     if (nav.classList.contains('disp') == true ) { 
         nav.classList.remove('disp');
+        navButton.style.backgroundColor = 'white';
+        navButton.style.color = 'black';
+        navButton.style.borderRadius = '15px';
     } else {
         nav.classList.add('disp');
+        navButton.style.backgroundColor = '';
+        navButton.style.color = '';
     }
 })
