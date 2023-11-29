8a3cf964449de6cbc25079124bc5a08f46015858
Kirunin <kirunin1@yandex.com>
2023-11-29 09:01:17 +0300

    head change

--- js/header.js
+++ js/header.js
@@ -4,28 +4,11 @@ const menu = document.querySelector('#menu');
 menuButton.addEventListener('click', () => {
     if (menu.classList.contains('disp') == true ) { 
         menu.classList.remove('disp');
-        menuButton.style.backgroundColor = 'white';
-        menuButton.style.color = 'black';
-        menuButton.style.borderRadius = '15px';
+        menuButton.classList.add('adaptive__burger-active');
     } else {
         menu.classList.add ('disp');
-        menuButton.style.backgroundColor = '';
-        menuButton.style.color = '';
+        menuButton.classList.remove('adaptive__burger-active');
     }
 })
 
-const navButton = document.querySelector('#navButton');
-const nav = document.querySelector('#nav');
 
-navButton.addEventListener('click', () => {
-    if (nav.classList.contains('disp') == true ) { 
-        nav.classList.remove('disp');
-        navButton.style.backgroundColor = 'white';
-        navButton.style.color = 'black';
-        navButton.style.borderRadius = '15px';
-    } else {
-        nav.classList.add('disp');
-        navButton.style.backgroundColor = '';
-        navButton.style.color = '';
-    }
-})
