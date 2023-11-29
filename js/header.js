f4d6507fcf20db8705fa7726b2f58bf06ac14eca
Kirunin <kirunin1@yandex.com>
2023-11-29 18:37:30 +0300

    add button up top

--- js/header.js
+++ js/header.js
@@ -1,13 +1,16 @@
 const menuButton = document.querySelector('#menuButton');
 const menu = document.querySelector('#menu');
+const goTop = document.querySelector('#goTop')
 
 menuButton.addEventListener('click', () => {
     if (menu.classList.contains('disp') == true ) { 
         menu.classList.remove('disp');
         menuButton.classList.add('adaptive__burger-active');
+        goTop.classList.remove('go-top');
     } else {
         menu.classList.add ('disp');
         menuButton.classList.remove('adaptive__burger-active');
+        goTop.classList.add('go-top');
     }
 })
 
