2bb25ec1477851ee4c14f9c49a2e692f70b1c90d
Kirunin <kirunin1@yandex.com>
2023-12-20 03:44:30 +0300

    edit wrapper in dark theme

--- js/main.js
+++ js/main.js
@@ -23,41 +23,160 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+// document.addEventListener('DOMContentLoaded', function () {
+// // Получение элемента .wrapper-background
+// const wrapperBackground = document.querySelector('.wrapper-background');
+
+// // Получение значения из localStorage
+// const isDarkMode = localStorage.getItem('darkmode') === 'true';
+
+// // Установка класса в зависимости от темы
+// wrapperBackground.classList.toggle('dark-mode', isDarkMode);
+
+// // Получение элементов темы
+// const sunThemeSwitch = document.querySelector('.header__theme-switch_sun');
+// const nightThemeSwitch = document.querySelector('.header__theme-switch_night');
+
+// // Установка видимости элементов в зависимости от текущей темы
+// sunThemeSwitch.style.display = isDarkMode ? 'none' : 'flex';
+// nightThemeSwitch.style.display = isDarkMode ? 'flex' : 'none';
+// });
+  
+// function darkMode() {
+// const wrapperBackground = document.querySelector('.wrapper-background');
+// const sunThemeSwitch = document.querySelector('.header__theme-switch_sun');
+// const nightThemeSwitch = document.querySelector('.header__theme-switch_night');
+// const wasDarkMode = localStorage.getItem('darkmode') === 'true';
+
+// // Изменение темы
+// localStorage.setItem('darkmode', !wasDarkMode);
+// wrapperBackground.classList.toggle('dark-mode', !wasDarkMode);
+
+// // Переключение между изображениями солнца и луны
+// sunThemeSwitch.style.display = !wasDarkMode ? 'none' : 'flex';
+// nightThemeSwitch.style.display = !wasDarkMode ? 'flex' : 'none';
+// }
+  
+// // Навешивание обработчика на переключатель темы
+// document.querySelector('.header__theme-switch_sun').addEventListener('click', darkMode);
+// document.querySelector('.header__theme-switch_night').addEventListener('click', darkMode);
+
 document.addEventListener('DOMContentLoaded', function () {
-// Получение элемента .wrapper-background
-const wrapperBackground = document.querySelector('.wrapper-background');
+    // Получение элемента .wrapper-background
+    const wrapperBackground = document.querySelector('.wrapper-background');
 
-// Получение значения из localStorage
-const isDarkMode = localStorage.getItem('darkmode') === 'true';
+    // Получение значения из localStorage
+    const isDarkMode = localStorage.getItem('darkmode') === 'true';
 
-// Установка класса в зависимости от темы
-wrapperBackground.classList.toggle('dark-mode', isDarkMode);
+    // Установка класса в зависимости от темы
+    wrapperBackground.classList.toggle('dark-mode', isDarkMode);
 
-// Получение элементов темы
-const sunThemeSwitch = document.querySelector('.header__theme-switch_sun');
-const nightThemeSwitch = document.querySelector('.header__theme-switch_night');
+    // Получение элементов темы
+    const sunThemeSwitch = document.querySelector('.header__theme-switch_sun');
+    const nightThemeSwitch = document.querySelector('.header__theme-switch_night');
 
-// Установка видимости элементов в зависимости от текущей темы
-sunThemeSwitch.style.display = isDarkMode ? 'none' : 'flex';
-nightThemeSwitch.style.display = isDarkMode ? 'flex' : 'none';
+    // Установка видимости элементов в зависимости от текущей темы
+    sunThemeSwitch.style.display = isDarkMode ? 'none' : 'flex';
+    nightThemeSwitch.style.display = isDarkMode ? 'flex' : 'none';
+
+    // Обновление стилей для блоков кода
+    updateCodeBlockStyles(isDarkMode);
 });
-  
+
 function darkMode() {
-const wrapperBackground = document.querySelector('.wrapper-background');
-const sunThemeSwitch = document.querySelector('.header__theme-switch_sun');
-const nightThemeSwitch = document.querySelector('.header__theme-switch_night');
-const wasDarkMode = localStorage.getItem('darkmode') === 'true';
+    const wrapperBackground = document.querySelector('.wrapper-background');
+    const sunThemeSwitch = document.querySelector('.header__theme-switch_sun');
+    const nightThemeSwitch = document.querySelector('.header__theme-switch_night');
+    const isDarkMode = localStorage.getItem('darkmode') === 'true';
 
-// Изменение темы
-localStorage.setItem('darkmode', !wasDarkMode);
-wrapperBackground.classList.toggle('dark-mode', !wasDarkMode);
+    // Изменение темы
+    localStorage.setItem('darkmode', String(!isDarkMode));
+    wrapperBackground.classList.toggle('dark-mode', !isDarkMode);
 
-// Переключение между изображениями солнца и луны
-sunThemeSwitch.style.display = !wasDarkMode ? 'none' : 'flex';
-nightThemeSwitch.style.display = !wasDarkMode ? 'flex' : 'none';
+    // Переключение между изображениями солнца и луны
+    sunThemeSwitch.style.display = isDarkMode ? 'flex' : 'none';
+    nightThemeSwitch.style.display = isDarkMode ? 'none' : 'flex';
+
+    // Обновление стилей для блоков кода
+    updateCodeBlockStyles(!isDarkMode);
 }
-  
+
 // Навешивание обработчика на переключатель темы
 document.querySelector('.header__theme-switch_sun').addEventListener('click', darkMode);
 document.querySelector('.header__theme-switch_night').addEventListener('click', darkMode);
+
+// Функция для обновления стилей блоков кода
+function updateCodeBlockStyles(isDarkMode) {
+    const codeHtmlParagraphSelectors = ['.info-paragraph_code-html'];
+    const codeCssParagraphSelectors = ['.info-paragraph_code-css'];
+    const codeHtmlBlockSelectors = ['.info-block_code-html'];
+    const codeCssBlockSelectors = ['.info-block_code-css'];
+    const codeTextHtmlSelectors = ['.info-html-text_dark-theme'];
+
+// Обновление стилей для кода CSS
+codeHtmlParagraphSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-paragraph_code-html_dark-theme', isDarkMode);
+    });
+});
+codeCssParagraphSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-paragraph_code-css_dark-theme', isDarkMode);
+    });
+});
+codeHtmlBlockSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-block_code-html_dark-theme', isDarkMode);
+    });
+});
+codeCssBlockSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-block_code-css_dark-theme', isDarkMode);
+    });
+});
+codeTextHtmlSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-html-text_dark-theme', isDarkMode);
+    });
+});
+
+const htmlParagraphSelectors = ['.info-paragraph_code-html'];
+const htmlBlockSelectors = ['.info-block_code-html'];
+const cssParagraphSelectors = ['.info-paragraph_code-css'];
+const cssBlockSelectors = ['.info-block_code-css'];
+const textHtmlSelectors = ['.info-html-text'];
+
+// Обновление стилей для кода HTML
+htmlParagraphSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-paragraph_code-html_dark-theme', isDarkMode);
+    });
+});
+
+htmlBlockSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-block_code-html_dark-theme', isDarkMode);
+    });
+});
+
+cssParagraphSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-paragraph_code-css_dark-theme', isDarkMode);
+    });
+});
+
+cssBlockSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-block_code-css_dark-theme', isDarkMode);
+    });
+});
+
+textHtmlSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(element => {
+        element.classList.toggle('info-html-text_dark-theme', isDarkMode);
+    });
+});
+}
+
+
 
