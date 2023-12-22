1b7eb983935d1a2129e605bb3ec410964d5c9255
Kirunin <kirunin1@yandex.com>
2023-12-22 11:47:06 +0300

    remove and fixed  js code for button go-top

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let scrollButton = document.getElementById('goTop');
-
-    // Показываем или скрываем кнопку при прокрутке
-    window.onscroll = function () {
-        if (document.body.scrollTop > document.documentElement.clientHeight / 2 ||
-            document.documentElement.scrollTop > document.documentElement.clientHeight / 2) {
-            scrollButton.style.display = 'flex';
-        } else {
-            scrollButton.style.display = 'none';
-        }
-    };
-
-    // Прокрутка вверх при клике на кнопку
-    scrollButton.onclick = function () {
-        scrollToTop();
-    };
-
-    // Функция прокрутки вверх
-    function scrollToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     // Получение элемента .wrapper-background
     const wrapperBackground = document.querySelector('.wrapper-background');
